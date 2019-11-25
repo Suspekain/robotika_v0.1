403fe72ca880e944eb5f470217dddd5c63f9bdee
Ekain Susperregi Ibarguren <suspekain@gmail.com>
2019-11-25 17:26:00 +0100

    First Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,200 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>RO-BOTIKA</title>
+
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Team</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        <div class="carousel slide" id="carousel-997670">
+          <ol class="carousel-indicators">
+            <li data-slide-to="0" data-target="#carousel-997670">
+            </li>
+            <li data-slide-to="1" data-target="#carousel-997670">
+            </li>
+            <li data-slide-to="2" data-target="#carousel-997670" class="active">
+            </li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item">
+              <img class="d-block w-100" alt="Carousel Bootstrap First" src="https://www.layoutit.com/img/sports-q-c-1600-500-1.jpg">
+              <div class="carousel-caption">
+                <h4>First image label</h4>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" alt="Carousel Bootstrap Second" src="https://www.layoutit.com/img/sports-q-c-1600-500-2.jpg">
+              <div class="carousel-caption">
+                <h4>Second image label</h4>
+              </div>
+            </div>
+            <div class="carousel-item active">
+              <img class="d-block w-100" alt="Carousel Bootstrap Third" src="https://www.layoutit.com/img/sports-q-c-1600-500-3.jpg">
+              <div class="carousel-caption">
+                <h4>Third image label</h4>
+              </div>
+            </div>
+          </div> <a class="carousel-control-prev" href="#carousel-997670" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carousel-997670" data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-md-4">
+        <h2>Fast</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        </p>
+        <p>
+          <a class="btn" href="#">View details »</a>
+        </p>
+      </div>
+      <div class="col-md-4">
+        <div class="jumbotron">
+          <h2>
+            Hire our service now!
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.          </p>
+            <p>
+              <a class="btn btn-primary btn-large" href="#">Contract us</a>
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <h2>
+            Secure
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </p>
+          <p>
+            <a class="btn" href="#">View details »</a>
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-8">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="card bg-default">
+                <h5 class="card-header">
+                  Xabi Lazkano
+                </h5>
+                <div class="card-body">
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  </p>
+                </div>
+                <div class="card-footer">
+                  COO
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card">
+                <h5 class="card-header">
+                  Ander Gonzalez
+                </h5>
+                <div class="card-body">
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  </p>
+                </div>
+                <div class="card-footer">
+                  CTO
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card">
+                <h5 class="card-header">
+                  Ekain Susperregi
+                </h5>
+                <div class="card-body">
+                  <p class="card-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                  </p>
+                </div>
+                <div class="card-footer">
+                  CEO
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <form role="form">
+            <div class="form-group">
+              <label for="exampleInputPassword1">
+                Nombre
+              </label>
+              <input type="text" class="form-control" id="nombre">
+
+              <label>
+                Email address
+              </label>
+              <input type="email" class="form-control" id="email">
+            </div>
+            <div class="form-group">
+
+              <label>
+                Mensaje
+              </label>
+              <input type="text" class="form-control" id="mensaje">
+            </div>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </div>
+
+
+        </form>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="row">
+          <a href="https://www.facebook.com"><img style="width: 100px" src="img/facebook.png" alt=""></a>
+          <a href="https://www.twitter.com"><img style="width: 100px" src="img/twitter.png" alt=""></a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
